1da5f4574486532577b42fe48a7538e8b8439dc1
Aniket-Agalave <aniketsa4510@gmail.com>
2025-05-14 09:31:04 +0530

    authenticated user with jdbc

--- src/main/java/com/tka/LoginServlet.java
+++ src/main/java/com/tka/LoginServlet.java
@@ -1,6 +1,9 @@
 package com.tka;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,37 +12,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/login-user")
 public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
-		if("admin".equals(username) && "123".equals(password)) {
-			RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
-			rd.forward(request, response);
-		}else {
-			
-			request.setAttribute("msg", "invalid");
-			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-			rd.forward(request, response);
-			
+
+		try {
+			String query = "SELECT * FROM user WHERE username=? and password=?";
+			Connection connection = DBConnection.getConnection();
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setString(1, username);
+			ps.setString(2, password);
+
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+				rd.forward(request, response);
+			} else {
+				request.setAttribute("msg", "invalid");
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request, response);
+
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
-		
-		
-        System.out.println("in get");
+
+//		if("admin".equals(username) && "123".equals(password)) {
+//			RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+//			rd.forward(request, response);
+//		}else {
+//			
+//			request.setAttribute("msg", "invalid");
+//			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+//			rd.forward(request, response);
+//			
+//		}
+
+		System.out.println("in get");
 	}
-	
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    doPost(request, response); // forward GET requests to POST logic
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response); // forward GET requests to POST logic
 	}
-
-	
-
 
 }
